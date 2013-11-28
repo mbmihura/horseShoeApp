67ad5af242ece8fc21415877f8875722f5021bf5
Martin Mihura <mbmihura@gmail.com>
2013-11-28 14:45:32 -0300

    move metroExample page to its own view.

--- app/js/app.js
+++ app/js/app.js
@@ -13,5 +13,6 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/bigExamples', {templateUrl: 'partials/bigExample.html'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
