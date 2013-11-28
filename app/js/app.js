9fc295de56e5a963b9fcab7a4e95b725ebac0fda
Martin Mihura <mbmihura@gmail.com>
2013-11-28 15:37:40 -0300

    Adds reference to angular-ui-bootstrap-addOns js file in index, and to angular module un app.js.

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.bootstrapAddOns.collapse'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
