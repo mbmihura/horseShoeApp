040b68a338ac51905847e3ad4163a9d08bbf7a50
Martin Mihura <mbmihura@gmail.com>
2013-11-29 09:49:54 -0300

    Implements collapsableelement directive to beused in metro' side-nav-bar.

--- app/js/app.js
+++ app/js/app.js
@@ -14,6 +14,48 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl2'});
   $routeProvider.when('/bigExamples', {templateUrl: 'partials/bigExample.html'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
+
+
+angular.module('plunker', ['ui.bootstrap']);
+function CollapseDemoCtrl($scope) {
+  $scope.isCollapsed = false;
+}
+
+function AccordionDemoCtrl($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: "Dynamic Group Header - 1",
+      content: "Dynamic Group Body - 1"
+    },
+    {
+      title: "Dynamic Group Header - 2",
+      content: "Dynamic Group Body - 2"
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+}
+
+function DropdownCtrl($scope) {
+  $scope.items = [
+    "The first choice!",
+    "And another choice for you.",
+    "but wait! A third!"
+  ];
+}
+function TypeaheadCtrl($scope) {
+
+  $scope.selected = undefined;
+  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+}
--- app/lib/angular-ui-bootstrap-addOns/ui-bootstrap-addOns-0.1.0.js
+++ app/lib/angular-ui-bootstrap-addOns/ui-bootstrap-addOns-0.1.0.js
@@ -1,6 +1,9 @@
 angular.module('ui.bootstrapAddOns.collapse',['ui.bootstrap.transition'])
 
-.directive('collapsableelement', function() {
+.directive('collapsableelement', function($document, $location) {
+    var openElement;
+    var closeActive = angular.noop;
+
     return {
       restrict: 'E',
       replace: true,
@@ -10,14 +13,69 @@ angular.module('ui.bootstrapAddOns.collapse',['ui.bootstrap.transition'])
       transclude: true,
       template: '<div class="collapse" collapse="isCollapse" ng-transclude></div>',
       link: function(scope, element, attrs) {
+        // TODO: Offer posibility to config if element should automatically collapse when clicking out side or in other comtroller.
+        //var documentCliclHandler =  function (event) {
+        //    closeActive(event)
+        //    scope.$digest();
+        //  };
+
         scope.name = {
-          toggleState: function() {
-  	        scope.isCollapse = !scope.isCollapse;
-  	      }, showExpanded: function(show) {
-  	        scope.isCollapse = !!show;
+          collapse: function(event) {closeActive(event)},
+          expand: function(event)
+          { 
+            var elementWasOpen = (element === openElement);
+
+            // The default action of the event will not be triggered (e.g. clicked anchors will not take the browser to a new URL) and prevented from bubbling up the DOM tree, preventing any parent handlers from being notified of the event.
+            if (event) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+
+            if (!!openElement)
+            {
+              closeActive(event)
+            }
+            
+            if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) { 
+              scope.isCollapse = false;
+              openElement = element;
+              closeActive = function (event) {
+                // The default action of the event will not be triggered (e.g. clicked anchors will not take the browser to a new URL) and prevented from bubbling up the DOM tree, preventing any parent handlers from being notified of the event.
+                if (event) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                // collapse element
+                scope.isCollapse = true;
+
+
+                // remove function to collapse element when clicking outside of it and deletes function.
+                // $document.unbind('click',  documentCliclHandler);
+                closeActive = angular.noop;
+                openElement = null;
+              };
+              //$document.bind('click', documentCliclHandler);
+            };
+          },
+
+
+
+          toggleState: function(event) {
+            if (scope.isCollapse) {
+              scope.name.expand(event);
+            } else {
+              scope.name.collapse(event);
+            }
   	      }
   	    };
-        scope.name.showExpanded(true);
+        scope.collapseApply = function(event) {
+          scope.$apply(scope.name.collapse(event));
+        };
+
+        // Initially show element collapsed
+        scope.isCollapse = true;
+        scope.$watch('$location.path', function() { scope.name.collapse(); });
+        //element.parent().bind('click', function() { alert(); scope.name.collapse(); });
       }
     };
   });
