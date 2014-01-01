9a3215c2fdf34b800cc11f676cb1a75041779c56
Martin Mihura <mbmihura@gmail.com>
2014-01-01 15:19:47 -0300

    Adds owner list and edit views.

--- app/js/app.js
+++ app/js/app.js
@@ -12,6 +12,10 @@ angular.module('myApp', [
   'ui.bootstrapAddOns.collapse'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/horses', {templateUrl: 'partials/horsesList.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/owners', {templateUrl: 'partials/ownersList.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/owners/:id', {templateUrl: 'partials/ownerEdit.html', controller: 'MyCtrl2'});
+  
   $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl2'});
