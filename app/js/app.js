a8d345804dfd34a5ce5ef640cc3f529e7ce05e62
Martin Mihura <mbmihura@gmail.com>
2014-01-02 18:08:52 -0300

    Adds horse's details partials view.

--- app/js/app.js
+++ app/js/app.js
@@ -13,10 +13,11 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/horses', {templateUrl: 'partials/horsesList.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/horses/:id', {templateUrl: 'partials/horsesDetails.html', controller: 'MyCtrl2'});
   $routeProvider.when('/owners', {templateUrl: 'partials/ownersList.html', controller: 'MyCtrl2'});
   $routeProvider.when('/owners/:id', {templateUrl: 'partials/ownerDetails.html', controller: 'MyCtrl2'});
   $routeProvider.when('/owners/:id/edit', {templateUrl: 'partials/ownerEdit.html', controller: 'MyCtrl2'});
-  
+    
   $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl2'});
